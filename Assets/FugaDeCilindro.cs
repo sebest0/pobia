1d84d23aa8d32a1396826d05159f233dbf7e50e5
LucaVallazza <luvallazza@gmail.com>
2022-08-05 12:13:43 -0300

    Escape Senoidal

---
+++ Assets/FugaDeCilindro.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FugaDeCilindro : MonoBehaviour
+{
+    [SerializeField]
+    float i = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * Mathf.Sin(Time.time + i) * 0.01f;
+    }
+}
