b0b8bdb9b2f8e7cb012037a5df73ecd0db861c05
sebest0 <sebbest0@gmail.com>
2022-08-05 22:47:13 -0300

    el movimiento del mouse me va a obligar a saltar

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,17 +6,51 @@ using UnityEngine;
 public class Player : NetworkBehaviour
 {
     public float speed = 8f;
-    void Movimiento()
+    public float sensitivity = 100f;
+    public Camera cam;
+
+    float xRotation = 0;
+
+    private void Start()
     {
         if(isLocalPlayer)
         {
-            Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal"); //Movimiento en funcion de la orientacion
-            transform.position = Time.deltaTime * speed * move + transform.position;                                      //AAA
+            //Cuando aparece el jugador le saco el mouse dea
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            //busco la camara cuando prenda el juego
+            cam = Camera.FindObjectOfType<Camera>();
+            //Pongo la camara encima del jugador y le digo que herbert es el papa
+            cam.transform.SetPositionAndRotation(transform.localPosition, transform.localRotation);
+            cam.transform.SetParent(transform);
         }
     }
-
-    void Update()
+    private void Update()
     {
         Movimiento();
     }
+
+    void Movimiento()
+    {
+        if (isLocalPlayer)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * sensitivity;
+            float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * sensitivity;
+
+            //El movimiento vertical del mouse hace girar en torno al eje X
+            xRotation -= mouseY;
+            //Para que no puedas girar
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+            cam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+            //Roto hacia la izquierda o derecha (sobre el eje Y)
+            transform.Rotate(Vector3.up * mouseX);
+
+            //Movimiento en funcion de la orientacion
+            Vector3 move = transform.forward * Input.GetAxis("Vertical") + transform.right * Input.GetAxis("Horizontal");
+            transform.position = Time.deltaTime * speed * move + transform.position;
+        }
+    }
 }
+
+
